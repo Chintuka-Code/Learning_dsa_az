232fddd4cbb143530945194f2f95cbeb23f62f92
Sabban <mohmad.sabban123@gmail.com>
2025-05-23 14:29:49 +0530

    feat: diameter of bt

--- src/max_height.ts
+++ src/max_height.ts
@@ -15,5 +15,13 @@ export function maxHeight(node: Node | null) {
 
   const leftHeight = maxHeight(node.left) as number;
   const rightHeight = maxHeight(node.right) as number;
+
+  // For dimaeter, we can also calculate the diameter at this node
+  // const diameter = leftHeight + rightHeight + 1;
+
   return 1 + Math.max(leftHeight, rightHeight);
 }
+
+// THis approach is also used to find the diameter of a binary tree.
+// The diameter of a binary tree is the length of the longest path between any two nodes in the tree.
+// The diameter can be found by calculating the height of the left and right subtrees for each node and keeping track of the maximum diameter found so far.
